5236d224382e87dcadd369dad56bb619a9371890
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-25 23:26:24 +0530

    added programme on string arguments based on team selection

---
+++ CricketersSA.java
@@ -0,0 +1,11 @@
+class CricketersSA{
+	public static void main (String [] squad){
+		System.out.println("Squad for Tour of South africa is");
+			if(squad.length>0){
+				for (int player=0; player<squad.length; player++){
+				System.out.println(squad[player]);
+				}
+			}
+		System.out.println("Stand by players will be announced shortly");
+	}
+}
